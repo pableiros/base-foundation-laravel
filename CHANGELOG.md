c12127c9de1fc5a4c7805a481edb5c927ff5ee92
Pablo Bórquez Urquidy <pablo.borquez.urquidy@gmail.com>
2023-07-15 12:46:52 -0700

    CSVSeeder implemented

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.4.0
+
+- `CSVSeeder` implemented.
+
 # 0.3.0
 
 - `BaseUserFormRequest` implemented.
---
+++ src/Seeders/CSVSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+abstract class CSVSeeder extends Seeder
+{
+    abstract protected function getCSVName();
+    abstract protected function getTableName();
+    abstract protected function getInsert($array);
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $filename = storage_path('csv/' . $this->getCSVName());
+        $csv = fopen($filename, 'r');
+        $tableName = $this->getTableName();
+
+        while(($array = fgetcsv($csv, 200, ',')) != false) {
+            $exists = DB::table($tableName)
+                ->where(['id' => $array[0]])
+                ->first() != null;
+
+            if ($exists == false) {
+                $insert = $this->getInsert($array);
+                $insert = array_merge($insert,
+                    [
+                        'created_at' => date("Y-m-d H:i:s"),
+                        'updated_at' => date("Y-m-d H:i:s")
+                    ]
+                );
+
+                DB::table($tableName)->insert($insert);
+            }
+        }
+    }
+}
